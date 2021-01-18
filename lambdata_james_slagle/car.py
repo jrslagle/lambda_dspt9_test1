47f797aa52240c1b58b509f9f1f98e66314e9ad2
jrslagle <jrslagle@gmail.com>
2021-01-17 22:34:13 -0700

    added a Car class

---
+++ lambdata_james_slagle/car.py
@@ -0,0 +1,40 @@
+
+class Car():
+    def __init__(self, mpg=30, tank_size_gallons=12):
+        self.mpg = mpg
+        self.tank_size_gallons = tank_size_gallons
+        self.odometer = 0
+        self.gallons_left = 12
+    
+    def drive(self, distance):
+        if self.gallons_left <= 0:
+            assert False, "You are out of gas and cannot drive"
+        else:
+            fuel_needed = distance / self.mpg
+            if fuel_needed <= self.gallons_left:
+                self.gallons_left -= fuel_needed
+                self.odometer += distance
+                print(f'You went {distance} miles and have {self.gallons_left:.1f} gallons of fuel left.')
+            else:
+                distance_possible = self.mpg * self.gallons_left
+                self.gallons_left = 0
+                self.odometer += distance_possible
+                print(f'You only went {distance_possible:.1f} miles and then ran out of gas.')
+    
+    def refuel(self, add_gallons):
+        if add_gallons > 0:
+            if (self.gallons_left + add_gallons) > self.tank_size_gallons:  # too much fuel
+                self.gallons_left = self.tank_size_gallons
+            else:
+                self.gallons_left += add_gallons
+            print(f"Refueled the tank to {self.gallons_left} gallons.")
+        else:
+            assert False, f"Adding {add_gallons} gallons to the tank makes no sense."
+
+if __name__ == '__main__':
+    my_car = Car(mpg=30, tank_size_gallons=12)
+    while my_car.gallons_left > 0:
+        my_car.drive(50)
+    my_car.refuel(14)
+    while my_car.gallons_left > 0:
+        my_car.drive(30)
--- my_script.py
+++ my_script.py
@@ -13,10 +13,19 @@ import lambdata_james_slagle.my_mod as js
 # penguins.dropna(inplace=True)
 # print(f"Null values after removing = {js.null_count(penguins)}")
 
-# # Demo split_dates(string/Datetime raw_dates)
+# # Demo year_month_day(string/Datetime raw_dates)
 # # Use Tanzania well data
 # DATA_PATH = 'https://raw.githubusercontent.com/LambdaSchool/DS-Unit-2-Kaggle-Challenge/master/data/'
 # train_val = pd.merge(pd.read_csv(DATA_PATH+'waterpumps/train_features.csv', na_values=[0, -2.000000e-08]),
 #                  pd.read_csv(DATA_PATH+'waterpumps/train_labels.csv')).set_index('id')
 # YMD = js.year_month_day(train_val.date_recorded)
 # print(YMD)
+
+# Demo my Car class
+from lambdata_james_slagle.car import Car
+my_car = Car(mpg=30, tank_size_gallons=12)
+while my_car.gallons_left > 0:
+    my_car.drive(50)
+my_car.refuel(14)
+while my_car.gallons_left > 0:
+    my_car.drive(30)
