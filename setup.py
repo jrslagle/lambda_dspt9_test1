ab9af518acb61a54c361273572db9ecefe9299dd
jrslagle <jrslagle@gmail.com>
2021-01-17 22:50:36 -0700

    changed version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="lambdata_james_slagle",  # Replace with your own username
-    version="0.0.1",
+    version="0.0.2",
     author="James Slagle",
     author_email="jrslagle@gmail.com",
     description="A small example package for Lambda homework",
